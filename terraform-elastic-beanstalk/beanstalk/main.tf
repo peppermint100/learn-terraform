92a82aaae93b050b5379e814f69bc566fb583b7f
ingyulee <ingyulee@tnear.com>
2023-01-11 16:49:40 +0900

    elastic beanstalk settings

--- terraform-elastic-beanstalk/beanstalk/main.tf
+++ terraform-elastic-beanstalk/beanstalk/main.tf
@@ -14,16 +14,131 @@ resource "aws_elastic_beanstalk_environment" "this" {
   solution_stack_name = "64bit Amazon Linux 2 v3.5.3 running Docker"
   tier = "WebServer"
 
+  # Instance
+  setting {
+    name = "SecurityGroups"
+    namespace = "aws:autoscaling:launchconfiguration"
+    value = var.security_groups
+  }
+
+  # Capacity
+  setting {
+    name      = "EnvironmentType"
+    namespace = "aws:elasticbeanstalk:environment"
+    value     = "LoadBalanced"
+  }
+
+  setting {
+    name      = "LoadBalancerType"
+    namespace = "aws:elasticbeanstalk:environment"
+    value     = "application"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name = "MinSize"
+    value = 1
+  }
+
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name = "MaxSize"
+    value = 2
+  }
+
+  setting {
+    name      = "LoadBalancerIsShared"
+    namespace = "aws:elasticbeanstalk:environment"
+    value     = "true"
+  }
+
+  setting {
+    name      = "InstanceTypes"
+    namespace = "aws:ec2:instances"
+    value     = "t3.micro"
+  }
+
+  setting {
+    name      = "ImageId"
+    namespace = "aws:autoscaling:launchconfiguration"
+    value     = var.ami_id
+  }
+
+  # Load Balancer
+  setting {
+    name      = "MatcherHTTPCode"
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    value     = 200
+  }
+
+  setting {
+    name      = "HealthCheckPath"
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    value     = var.health_check_path
+  }
+
+  # Rolling updates and deployments
+  setting {
+    name      = "DeploymentPolicy"
+    namespace = "aws:elasticbeanstalk:command"
+    value     = "Rolling"
+  }
+
+  setting {
+    name      = "BatchSizeType"
+    namespace = "aws:elasticbeanstalk:command"
+    value     = "Percentage"
+  }
+
+  setting {
+    name      = "BatchSize"
+    namespace = "aws:elasticbeanstalk:command"
+    value     = 50
+  }
+
+  setting {
+    name      = "RollingUpdateType"
+    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
+    value     = "Health"
+  }
+
+  # security
+  setting {
+    name      = "ServiceRole"
+    namespace = "aws:elasticbeanstalk:environment"
+    value     = var.service_role_arn
+  }
+
+  setting {
+    name      = "EC2KeyName"
+    namespace = "aws:autoscaling:launchconfiguration"
+    value     =  var.key_name
+  }
+
+  setting {
+    name      = "IamInstanceProfile"
+    namespace = "aws:autoscaling:launchconfiguration"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+
+  # Notifications
+  setting {
+    name      = "NotificationEndpoint"
+    namespace = "aws:elasticbeanstalk:sns:topics"
+    value     = var.notification_endpoint_email
+  }
+
+  # Network
   setting {
     namespace = "aws:ec2:vpc"
     name      = "VPCId"
-    value     = var.vpc_id 
+    value     = var.vpc_id
   }
 
   setting {
     namespace = "aws:ec2:vpc"
     name      = "Subnets"
-    value     = var.subnet_ids 
+    value     = var.subnet_ids
   }
 }
 
--- terraform-elastic-beanstalk/beanstalk/variables.tf
+++ terraform-elastic-beanstalk/beanstalk/variables.tf
@@ -13,3 +13,27 @@ variable "vpc_id" {
 variable "subnet_ids" {
     type = list(string)
 }
+
+variable "ami_id" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "security_groups" {
+    type = list(string)
+}
+
+variable "health_check_path" {
+    type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "notification_endpoint_email" {
+  type = string
+}
